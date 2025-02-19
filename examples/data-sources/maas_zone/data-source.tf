37afc9fc395bbc5b4ae7306c384790ade1090a7f
Pedro Castillo <pedro.castillo@canonical.com>
2025-02-19 23:49:53 +0000

    Add examples and generate docs for maas_zone

---
+++ examples/data-sources/maas_zone/data-source.tf
@@ -0,0 +1,8 @@
+data "maas_zone" "test_zone" {
+  description = "A description of the test zone"
+  name        = "test-zone"
+}
+
+data "maas_zone" "test_zone" {
+  name = maas_zone.test_zone.name
+}
